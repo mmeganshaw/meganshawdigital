1c2a1ae6d62edc051476cac20203b818624e3b98
Megan Shaw <meganrose@Megans-MacBook-Air.local>
2020-04-02 19:23:02 -0400

    Server code wriiten

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(expres.static(__dirname + "/public"));
+
+require("./routes/index.js")(app);
+
+app.listen(PORT, function () {
+    console.log("Server listening on port" + PORT)
+})
